6e9bbe0d26f9a5dcef756d70c798ff472355fed5
Blastanaut <6893703+Blastanaut@users.noreply.github.com>
2024-11-15 12:36:33 +0000

    Requirements fix

--- surfClassNotifier.js
+++ surfClassNotifier.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const config = require('./config');
 const moment = require('moment');
 const { initializeDatabase, getClassData, saveClassData } = require('./database');
